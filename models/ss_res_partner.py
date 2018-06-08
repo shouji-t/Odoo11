9af8386b1eeba7a4660d2d4ca750957520e03ee0
msd-suli <31750090+msd-suli@users.noreply.github.com>
2018-06-08 16:09:33 +0900

    suli0525

--- models/ss_res_partner.py
+++ models/ss_res_partner.py
@@ -9,22 +9,23 @@
 
 from odoo import models, fields, api
 
+
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     x_partner_cd = fields.Char('顧客コード')
 
-    #SsPj class Many2one - David Tang
-    #１対多項目のため、目的モデルで正反対の多対１関係を実装する
+    # SsPj class Many2one - David Tang
+    # １対多項目のため、目的モデルで正反対の多対１関係を実装する
     pj_ids = fields.One2many('ss.pj', 'partner_id', 'Pj')
 
-    x_regdate = fields.Datetime('申請日', default=fields.Datetime.now, required=True, readonly=True, copy=False)
+    x_regdate = fields.Datetime('申請日', default=fields.Datetime.now, required=True)
     x_regstatus = fields.Selection([
         ('regist', u'申請中'),
         ('process', u'処理中'),
         ('done', u'終了'),
-        ('cancel', u'廃棄'),
-        ], string='新規申請状態', default='regist', copy=False)
+        ('cancel', u'廃棄')
+    ], string='新規申請状態', default='regist')
     x_contract = fields.Boolean('契約書', default=False)
     x_order = fields.Boolean('注文書', default=False)
     x_purchase = fields.Boolean('発注書', default=False)
